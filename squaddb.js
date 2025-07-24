7a6cd63c1f561492848e3e504db96520fdad5d54
Did You Get Sniped? <100443596+DidYouGetSniped@users.noreply.github.com>
2025-07-24 17:11:51 -0500

    Add files via upload

---
+++ squaddb.js
@@ -0,0 +1,8 @@
+export const squadDB = {
+    // Example Squads (keys are now case-sensitive):
+    "APG": {
+        discordLink: "https://discord.gg/Wb8eTc5HND",
+        bio: "This is a short bio for the APG squad."
+    }
+    // Add more squads here using the exact case-sensitive name as the key...
+};
